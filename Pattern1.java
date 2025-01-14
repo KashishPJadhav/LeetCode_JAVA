591a5909bb5269a2d8425a8c1d6ef755ae409ea4
Kashish Pradeep Jadhav <161619524+KashishPJadhav@users.noreply.github.com>
2025-01-14 09:51:51 -0800

    Add files via upload
    *
    **
    ***
    ****
    *****
    ******

---
+++ Pattern1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Pattern1
+{
+    public static void main(String[] args)
+    {
+        int n;
+        System.out.print("Enter : ");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        sc.close();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+
+    }
+}
